bdc0e35b304bc48ce4c3ff82810fb4402e8758a8
Ken Leung <kenleung5e28@gmail.com>
2022-07-04 17:26:32 +0800

    implement IntoResponse trait for ApiError enum

--- src/errors.rs
+++ src/errors.rs
@@ -1,7 +1,9 @@
 use axum::{
     http::StatusCode,
     response::{IntoResponse, Response},
+    Json,
 };
+use serde_json::json;
 
 #[derive(thiserror::Error, Debug)]
 pub enum ApiError {
@@ -14,5 +16,18 @@ pub enum ApiError {
 }
 
 impl IntoResponse for ApiError {
-    fn into_response(self) -> Response {}
+    fn into_response(self) -> Response {
+        let status_code = match &self {
+            ApiError::NotFound => StatusCode::NOT_FOUND,
+            ApiError::BadRequest => StatusCode::BAD_REQUEST,
+            ApiError::Internal(_) => StatusCode::INTERNAL_SERVER_ERROR,
+        };
+        (
+            status_code,
+            Json(json!({
+                "message": &self.to_string(),
+            })),
+        )
+            .into_response()
+    }
 }
