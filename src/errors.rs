c4a0828068028c3c77977d728564fb44cf70d1d4
Ken Leung <kenleung5e28@gmail.com>
2022-07-06 16:09:14 +0800

    refactor error messages

--- src/errors.rs
+++ src/errors.rs
@@ -7,20 +7,20 @@ use serde_json::json;
 
 #[derive(thiserror::Error, Debug)]
 pub enum ApiError {
-    #[error("request path not found")]
-    NotFound,
+    #[error("resource not found: {0}")]
+    NotFound(String),
     #[error("bad request")]
     BadRequest,
-    #[error(transparent)]
+    #[error("server error has occurred")]
     ServerError(#[from] anyhow::Error),
-    #[error("a database error has occurred")]
+    #[error("database error has occurred")]
     DatabaseError(#[from] sqlx::Error),
 }
 
 impl IntoResponse for ApiError {
     fn into_response(self) -> Response {
         let status_code = match &self {
-            ApiError::NotFound => StatusCode::NOT_FOUND,
+            ApiError::NotFound(_) => StatusCode::NOT_FOUND,
             ApiError::BadRequest => StatusCode::BAD_REQUEST,
             ApiError::ServerError(_) | ApiError::DatabaseError(_) => {
                 StatusCode::INTERNAL_SERVER_ERROR
