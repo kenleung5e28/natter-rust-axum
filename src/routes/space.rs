f93c6e7d99c70cb33611f66836356690e878322a
Ken Leung <kenleung5e28@gmail.com>
2022-07-06 16:09:26 +0800

    add post message route

--- src/routes/space.rs
+++ src/routes/space.rs
@@ -1,8 +1,7 @@
 use crate::routes::{ApiContext, ApiResult};
 use axum::{
-    extract::MatchedPath,
+    extract::{MatchedPath, Path},
     http::StatusCode,
-    response::IntoResponse,
     routing::{get, post},
     Extension, Json, Router,
 };
@@ -10,7 +9,10 @@ use serde::{Deserialize, Serialize};
 use sqlx::{query, query_scalar};
 
 pub fn router() -> Router {
-    Router::new().route("/", post(create_space))
+    Router::new().route("/", post(create_space)).nest(
+        "/:space_id/messages",
+        Router::new().route("/", post(post_message)),
+    )
 }
 
 #[derive(Deserialize)]
@@ -28,10 +30,10 @@ struct CreateSpaceBody {
 async fn create_space(
     ctx: Extension<ApiContext>,
     path: MatchedPath,
-    Json(req): Json<CreateSpacePayload>,
+    Json(payload): Json<CreateSpacePayload>,
 ) -> ApiResult<Json<CreateSpaceBody>> {
-    let name = req.name;
-    let owner = req.owner;
+    let name = payload.name;
+    let owner = payload.owner;
     let space_id = query_scalar!(
         "INSERT INTO spaces (name, owner) VALUES ($1, $2) RETURNING space_id",
         name,
@@ -44,6 +46,43 @@ async fn create_space(
         Json(CreateSpaceBody {
             name,
             uri: format!("{}/{}", path.as_str(), space_id),
+        }),
+    ))
+}
+
+#[derive(Deserialize)]
+struct PostMessagePayload {
+    author: String,
+    message: String,
+}
+
+#[derive(Serialize)]
+struct PostMessageBody {
+    uri: String,
+}
+
+async fn post_message(
+    ctx: Extension<ApiContext>,
+    Path(space_id): Path<i32>,
+    path: MatchedPath,
+    Json(payload): Json<PostMessagePayload>,
+) -> ApiResult<Json<PostMessageBody>> {
+    let author = payload.author;
+    let message = payload.message;
+    let msg_id = query_scalar!(
+        r#"
+        INSERT INTO messages (space_id, author, msg_text) VALUES ($1, $2, $3) RETURNING msg_id
+    "#,
+        space_id,
+        author,
+        message
+    )
+    .fetch_one(&ctx.db)
+    .await?;
+    Ok((
+        StatusCode::CREATED,
+        Json(PostMessageBody {
+            uri: format!("{}/{}", path.as_str(), msg_id),
         }),
     ))
 }
