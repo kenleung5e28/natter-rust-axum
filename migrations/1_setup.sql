800dd0fe0a17e9c4990fbba9b83805f49b6abc14
Ken Leung <kenleung5e28@gmail.com>
2022-07-08 17:23:20 +0800

    update setup sql

--- migrations/1_setup.sql
+++ migrations/1_setup.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS messages (
     space_id INT NOT NULL REFERENCES spaces(space_id),
     msg_id SERIAL PRIMARY KEY,
     author VARCHAR(30) NOT NULL,
-    msg_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    msg_time TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
     msg_text VARCHAR(1024) NOT NULL
 );
 CREATE INDEX IF NOT EXISTS msg_timestamp_idx ON messages(msg_time);
